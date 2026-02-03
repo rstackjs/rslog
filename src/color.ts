395be90f4772bfb9816dbe2abd639c33fc36825e
neverland <chenjiahan.jait@bytedance.com>
2026-02-03 14:14:09 +0800

    refactor: replacing custom formatter with styleText utility

--- src/color.ts
+++ src/color.ts
@@ -1,36 +1,16 @@
-import { colorLevel } from './utils.js';
+import { styleText } from 'node:util';
 
-export type ColorFn = (input: string | number | null | undefined) => string;
+export type ColorFn = (text: string | number) => string;
 
-let formatter = (open: string, close: string, replace = open): ColorFn =>
-  colorLevel >= 2
-    ? input => {
-        let string = '' + input;
-        let index = string.indexOf(close, open.length);
-        return ~index
-          ? open + replaceClose(string, close, replace, index) + close
-          : open + string + close;
-      }
-    : String;
+const createStyler =
+  (style: Parameters<typeof styleText>[0]): ColorFn =>
+  text =>
+    styleText(style, String(text));
 
-let replaceClose = (
-  string: string,
-  close: string,
-  replace: string,
-  index: number,
-): string => {
-  let start = string.substring(0, index) + replace;
-  let end = string.substring(index + close.length);
-  let nextIndex = end.indexOf(close);
-  return ~nextIndex
-    ? start + replaceClose(end, close, replace, nextIndex)
-    : start + end;
-};
-
-export const bold = formatter('\x1b[1m', '\x1b[22m', '\x1b[22m\x1b[1m');
-export const red = formatter('\x1b[31m', '\x1b[39m');
-export const green = formatter('\x1b[32m', '\x1b[39m');
-export const yellow = formatter('\x1b[33m', '\x1b[39m');
-export const magenta = formatter('\x1b[35m', '\x1b[39m');
-export const cyan = formatter('\x1b[36m', '\x1b[39m');
-export const gray = formatter('\x1b[90m', '\x1b[39m');
+export const bold = createStyler('bold');
+export const red = createStyler('red');
+export const green = createStyler('green');
+export const yellow = createStyler('yellow');
+export const magenta = createStyler('magenta');
+export const cyan = createStyler('cyan');
+export const gray = createStyler('gray');
