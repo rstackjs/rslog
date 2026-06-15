51747bc4401df23e68463e8e1e3f9f0bbf083d99
Jiahan Chen <chenjiahan.jait@bytedance.com>
2026-06-15 13:41:30 +0800

    chore: upgrade rslint to 0.6.1 (#91)

--- src/utils.ts
+++ src/utils.ts
@@ -3,7 +3,7 @@ import supportsColor from 'supports-color';
 // https://github.com/chalk/supports-color
 export const colorLevel = supportsColor.stdout ? supportsColor.stdout.level : 0;
 
-const errorStackRegExp = /at [^\r\n]{0,200}:\d+:\d+[\s\)]*$/;
+const errorStackRegExp = /at [^\r\n]{0,200}:\d+:\d+[\s)]*$/;
 const anonymousErrorStackRegExp = /at [^\r\n]{0,200}\(<anonymous>\)$/;
 const indexErrorStackRegExp = /at [^\r\n]{0,200}\(index\s\d+\)$/;
 
